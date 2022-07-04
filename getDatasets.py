21d81baf1594a171b7c677c68889cd1539d7bb63
linego <leonardo.zaffaroni@gmail.com>
2022-07-04 23:06:14 +0200

    Added Rapporti_lavoro_cessati.csv

--- getDatasets.py
+++ getDatasets.py
@@ -5,7 +5,7 @@ path = "./datasets/"
 
 dataset1 = "Rapporti_di_lavoro_attivati.csv"
 dataset2 = "6f0f7122-3312-47ac-a292-7a647986387e.csv"
-dataset3 = "DCSC_RETRATECO1_01072022002223923.csv"
+dataset3 = "Rapporti_di_lavoro_cessati.csv"
 
 if not os.path.exists(path):
     os.mkdir(path)
@@ -19,5 +19,7 @@ if not os.path.exists(path + dataset2):
     open(path + dataset2, 'wb').write(r.content)
 
 if not os.path.exists(path + dataset3):
-    r = requests.get("https://dati.lombardia.it/api/views/qbau-cyuc/rows.csv?accessType=DOWNLOAD", allow_redirects=True)
-    open(path + dataset3, 'wb').write(r.content)
+    r = requests.get("https://www.dati.lombardia.it/api/views/nwz3-p6vm/rows.csv?accessType=DOWNLOAD", allow_redirects=True)
+    open(path + dataset1, 'wb').write(r.content)
+
+
