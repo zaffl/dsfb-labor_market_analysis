c997bca7a5c664fec4297ccd624381c50f5c217c
zaffl <leonardo.zaffaroni@gmail.com>
2022-07-05 21:40:35 +0200

    Ateco Code - Some fix in join operation

--- getDatasets.py
+++ getDatasets.py
@@ -3,16 +3,17 @@ import requests
 
 path = "./datasets/"
 
-dataset1 = "Rapporti_di_lavoro_attivati.csv"
+dataset_att = "Rapporti_di_lavoro_attivati.csv"
 dataset2 = "6f0f7122-3312-47ac-a292-7a647986387e.csv"
 dataset3 = "Rapporti_di_lavoro_cessati.csv"
+dataset_geojson = "limits_IT_provinces.geojson"
 
 if not os.path.exists(path):
     os.mkdir(path)
 
-if not os.path.exists(path + dataset1):
+if not os.path.exists(path + dataset_att):
     r = requests.get("https://dati.lombardia.it/api/views/qbau-cyuc/rows.csv?accessType=DOWNLOAD", allow_redirects=True)
-    open(path + dataset1, 'wb').write(r.content)
+    open(path + dataset_att, 'wb').write(r.content)
 
 if not os.path.exists(path + dataset2):
     r = requests.get("https://indicepa.gov.it/ipa-dati/datastore/dump/6f0f7122-3312-47ac-a292-7a647986387e?bom=True", allow_redirects=True)
@@ -22,4 +23,8 @@ if not os.path.exists(path + dataset3):
     r = requests.get("https://www.dati.lombardia.it/api/views/nwz3-p6vm/rows.csv?accessType=DOWNLOAD", allow_redirects=True)
     open(path + dataset1, 'wb').write(r.content)
 
+# Retrive GeoJSON border coordinates of Italy provinces from github
+if not os.path.exists(path + dataset_geojson):
+    r = requests.get('https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_provinces.geojson')
+    open(path + dataset_geojson, 'wb').write(r.content)
 
