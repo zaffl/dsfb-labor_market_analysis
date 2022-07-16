45fda15d939d4d13b15d04935d804bece7167499
linego <leonardo.zaffaroni@gmail.com>
2022-07-16 16:25:23 +0200

    Slides update - data exploration and prediction

--- getDatasets.py
+++ getDatasets.py
@@ -5,31 +5,37 @@ import requests
 
 path = "./datasets/"
 
-dataset_att = "Rapporti_di_lavoro_attivati.csv"
-dataset2 = "6f0f7122-3312-47ac-a292-7a647986387e.csv"
-dataset3 = "Rapporti_di_lavoro_cessati.csv"
-dataset_geojson = "limits_IT_provinces.geojson"
+# Retriving all datasets from online sources if we don't have them
+import os
+import requests
 
-# Create dataset dir
+path = "./datasets/"
+
+datasets = [
+    {# Activated Contracts Dataset
+        'url' : 'https://dati.lombardia.it/api/views/qbau-cyuc/rows.csv?accessType=DOWNLOAD',
+        'filename' : 'Rapporti_di_lavoro_attivati.csv'
+    },
+    {# Ceased Contracts Dataset
+        'url' : 'https://www.dati.lombardia.it/api/views/nwz3-p6vm/rows.csv?accessType=DOWNLOAD',
+        'filename' : 'Rapporti_di_lavoro_cessati.csv'
+    },
+    {# ATECO Code Dataset
+        'url' : 'https://www.istat.it/it/files//2022/03/Struttura-ATECO-2007-aggiornamento-2022.xlsx',
+        'filename' : 'Struttura-ATECO-2007-aggiornamento-2022.xlsx'
+    },
+    {# GeoJSON border coordinates of Italy provinces from github
+        'url' : 'https://dati.lombardia.it/api/views/qbau-cyuc/rows.csv?accessType=DOWNLOAD',
+        'filename' : 'limits_IT_provinces.geojson'
+    },   
+]
+
+# Create datasets dir
 if not os.path.exists(path):
     os.mkdir(path)
 
-# Get Activated Contracts Dataset
-if not os.path.exists(path + dataset_att):
-    r = requests.get("https://dati.lombardia.it/api/views/qbau-cyuc/rows.csv?accessType=DOWNLOAD", allow_redirects=True)
-    open(path + dataset_att, 'wb').write(r.content)
-
-# Get Ceased Contracts Dataset
-if not os.path.exists(path + dataset3):
-    r = requests.get("https://www.dati.lombardia.it/api/views/nwz3-p6vm/rows.csv?accessType=DOWNLOAD", allow_redirects=True)
-    open(path + dataset1, 'wb').write(r.content)
-
-# Get ATECO Code Dataset
-if not os.path.exists(path + dataset2):
-    r = requests.get("https://indicepa.gov.it/ipa-dati/datastore/dump/6f0f7122-3312-47ac-a292-7a647986387e?bom=True", allow_redirects=True)
-    open(path + dataset2, 'wb').write(r.content)
-
-# Get GeoJSON border coordinates of Italy provinces from github
-if not os.path.exists(path + dataset_geojson):
-    r = requests.get('https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_provinces.geojson', allow_redirects=True)
-    open(path + dataset_geojson, 'wb').write(r.content)
+# Retrive datasets
+for dataset in datasets:
+    if not os.path.exists(path + dataset["filename"]):
+        r = requests.get(dataset["url"], allow_redirects=True)
+        open(path + dataset["filename"], 'wb').write(r.content)
